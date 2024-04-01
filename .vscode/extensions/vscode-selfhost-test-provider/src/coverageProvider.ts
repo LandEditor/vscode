6048c54a1f9dbf952859d8bd59b7069daec670e5
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-01 13:41:29 +0300

    squash!

--- .vscode/extensions/vscode-selfhost-test-provider/src/coverageProvider.ts
+++
@@ -1,7 +0,0 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-
-import { IstanbulCoverageContext } from 'istanbul-to-vscode';
-
-export const coverageContext = new IstanbulCoverageContext();
