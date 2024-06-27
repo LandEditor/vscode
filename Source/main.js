ae67a709572870a7901f234d0b868a86b27594b7
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-27 06:50:27 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/main.js
+++ Source/main.js
@@ -7,9 +7,9 @@
 'use strict';
 
 /**
- * @typedef {import('./vs/base/common/product').IProductConfiguration} IProductConfiguration
- * @typedef {import('./vs/base/node/languagePacks').NLSConfiguration} NLSConfiguration
- * @typedef {import('./vs/platform/environment/common/argv').NativeParsedArgs} NativeParsedArgs
+ * @import { IProductConfiguration } from './vs/base/common/product'
+ * @import { NLSConfiguration } from './vs/base/node/languagePacks'
+ * @import { NativeParsedArgs } from './vs/platform/environment/common/argv'
  */
 
 const perf = require('./vs/base/common/performance');
@@ -205,7 +205,10 @@ function configureCommandlineSwitchesSync(cliArgs) {
 		'force-color-profile',
 
 		// disable LCD font rendering, a Chromium flag
-		'disable-lcd-text'
+		'disable-lcd-text',
+
+		// bypass any specified proxy for the given semi-colon-separated list of hosts
+		'proxy-bypass-list'
 	];
 
 	if (process.platform === 'linux') {
@@ -243,10 +246,7 @@ function configureCommandlineSwitchesSync(cliArgs) {
 					app.commandLine.appendSwitch(argvKey);
 				}
 			} else if (argvValue) {
-				if (argvKey === 'force-color-profile') {
-					// Color profile
-					app.commandLine.appendSwitch(argvKey, argvValue);
-				} else if (argvKey === 'password-store') {
+				if (argvKey === 'password-store') {
 					// Password store
 					// TODO@TylerLeonhardt: Remove this migration in 3 months
 					let migratedArgvValue = argvValue;
@@ -254,6 +254,8 @@ function configureCommandlineSwitchesSync(cliArgs) {
 						migratedArgvValue = 'gnome-libsecret';
 					}
 					app.commandLine.appendSwitch(argvKey, migratedArgvValue);
+				} else {
+					app.commandLine.appendSwitch(argvKey, argvValue);
 				}
 			}
 		}
