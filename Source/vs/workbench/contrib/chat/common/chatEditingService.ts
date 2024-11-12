1921f18c94662806171c61db4f442bbba7b3c54f
Joyce Er <joyce.er@microsoft.com>
2024-11-12 14:08:34 -0800

    feat: allow chat related file providers to specify a description at registration and for results (#233731)

--- Source/vs/workbench/contrib/chat/common/chatEditingService.ts
+++ Source/vs/workbench/contrib/chat/common/chatEditingService.ts
@@ -41,7 +41,7 @@ export interface IChatEditingService {
 	restoreSnapshot(requestId: string | undefined): Promise<void>;
 
 	registerRelatedFilesProvider(handle: number, provider: IChatRelatedFilesProvider): IDisposable;
-	getRelatedFiles(chatSessionId: string, prompt: string, token: CancellationToken): Promise<readonly URI[] | undefined>;
+	getRelatedFiles(chatSessionId: string, prompt: string, token: CancellationToken): Promise<readonly IChatRelatedFile[] | undefined>;
 }
 
 export interface IChatRequestDraft {
@@ -49,8 +49,13 @@ export interface IChatRequestDraft {
 	readonly files: readonly URI[];
 }
 
+export interface IChatRelatedFile {
+	readonly uri: URI;
+	readonly description: string;
+}
+
 export interface IChatRelatedFilesProvider {
-	provideRelatedFiles(chatRequest: IChatRequestDraft, token: CancellationToken): Promise<URI[] | undefined>;
+	provideRelatedFiles(chatRequest: IChatRequestDraft, token: CancellationToken): Promise<IChatRelatedFile[] | undefined>;
 }
 export interface IChatEditingSession {
     readonly chatSessionId: string;
